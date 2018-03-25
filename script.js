7aec6fbec5883327ed49f70edaf45cd8034ef0a1
Chance Baars <chance@Chance.home>
2018-03-25 18:20:54 -0400

    halve way dubble click

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 jQuery(function() {
 
 
+
+  let count = 0;
+  let turn;
+
   $('.colom').on('click', function() {
-    $(this).addClass('playerX');
+    if (count % 2 === 0) {
+      $(this).addClass('playerX');
+      count++;
+      $('h2').text(`It's your turn '0'`);
+    } else if (!$(this).hasClass('playerO')) {
+      $(this).addClass('playerO');
+      count++;
+      $('h2').text(`It's your turn 'X'`);
+    }
+    console.log(count);
   });
 
+  $('button').on('click', function() {
+    $('div').removeClass('playerO playerX');
+    count = 0;
+    $('h2').text(`'X' can start!`);
+  });
 
 });
