f4c7bb753271fe63cdb1514ab1afdfb9e8d6ccc4
Chance Baars <chance@Chance.home>
2018-03-25 22:15:47 -0400

    Almost done

--- script.js
+++ script.js
@@ -3,19 +3,37 @@ jQuery(function() {
 
 
   let count = 0;
-  let turn;
+  let x = [];
+  let o = [];
+  let optionOne = [one,two,tree];
+  let optionTWo = [one,four,seven];
+  let optionThree = [four,five,six];
+  let optionFour = [seven,eigth,nine];
+  let optionFive = [two,five,eigth];
+  let optionSix = [tree,six,nine];
+  let optionSeven = [one,five,nine];
+  let optionEight = [tree,five,seven]
 
   $('.colom').on('click', function() {
-    if (count % 2 === 0) {
-      $(this).addClass('playerX');
-      count++;
-      $('h2').text(`It's your turn '0'`);
-    } else if (!$(this).hasClass('playerO')) {
-      $(this).addClass('playerO');
-      count++;
-      $('h2').text(`It's your turn 'X'`);
+
+    if (!$(this).hasClass('playerO') && !$(this).hasClass('playerX')) {
+      if (count % 2 === 0) {
+        $(this).addClass('playerX');
+        count++;
+        x.push(this.id);
+        $('h2').text(`It's your turn '0'`);
+      } else {
+        $(this).addClass('playerO');
+        count++;
+        o.push(this.id);
+        $('h2').text(`It's your turn 'X'`);
+      }
+    } else if (count === 9) {
+      alert('GAME IS OVER')
+    } else {
+      $('h2').text(`Find an empty box!`);
     }
-    console.log(count);
+    if ()
   });
 
   $('button').on('click', function() {
